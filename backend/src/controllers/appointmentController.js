b5463962ca6631769fcbd9c5057bf16408ec664f
Soumya yallappa belli <bellisoumya@gmail.com>
2026-06-29 14:41:09 +0530

    appointments

--- backend/src/controllers/appointmentController.js
+++ backend/src/controllers/appointmentController.js
@@ -1,4 +1,4 @@
-const Appointment = require('../models/Appointment')
+﻿const Appointment = require('../models/Appointment')
 const User = require('../models/User')
 const { isConnected } = require('../config/db')
 const { emitAppointmentUpdate, emitDashboardUpdate } = require('../utils/realtime')
